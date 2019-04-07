6b86b355808630ffc51186b794e478e36a11a00c
unknown <tonyc-cathy@tonyc-server.synacast.local>
2019-04-07 17:51:18 +0800

    .

--- vue-demo/vue-app/my-vue-app/src/main.js
+++ vue-demo/vue-app/my-vue-app/src/main.js
@@ -3,6 +3,7 @@
 import Vue from 'vue'
 // import App from './App'
 // import router from './router'
+import VeLine from 'v-charts/lib/line.common'
 
 Vue.config.productionTip = false
 
@@ -40,4 +41,24 @@ var app4 = new Vue({
       {text: 'C'}
     ]
   }
+})
+
+var app5 = new Vue({
+  el: '#app-5',
+  data: function () {
+    return {
+      chartData: {
+        columns: ['日期', '访问用户', '下单用户', '下单率'],
+        rows: [
+          { '日期': '1/1', '访问用户': 1393, '下单用户': 1093, '下单率': 0.32 },
+          { '日期': '1/2', '访问用户': 3530, '下单用户': 3230, '下单率': 0.26 },
+          { '日期': '1/3', '访问用户': 2923, '下单用户': 2623, '下单率': 0.76 },
+          { '日期': '1/4', '访问用户': 1723, '下单用户': 1423, '下单率': 0.49 },
+          { '日期': '1/5', '访问用户': 3792, '下单用户': 3492, '下单率': 0.323 },
+          { '日期': '1/6', '访问用户': 4593, '下单用户': 4293, '下单率': 0.78 }
+        ]
+      }
+    }
+  },
+  components: { VeLine }
 })
