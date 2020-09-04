07350863fc5b262b8e3df9fa7157e02e93da0ffe
chentoz <492309497@qq.com>
2020-09-04 16:22:53 +0800

    fix

--- sync-apidb-minutesly.sh
+++ sync-apidb-minutesly.sh
@@ -1,11 +1,9 @@
 # !/bin/bash
 set -e
 
-set > /tmp/set-output1
-echo "yes" >> /tmp/run.log
 PATH=/home/chentoz/Downloads/local-osmosis/bin:/usr/local/go/bin:/home/chentoz/Downloads/local-osmosis/bin:/usr/local/go/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin:/home/chentoz/go/bin:/home/chentoz/go/bin
-echo "no" >> /tmp/run.log
-echo $PATH > /tmp/path
+
+DEBUG=off
 
 NOW=$(date -u +"%Y-%m-%d_%H:%M:%S")
 
@@ -19,11 +17,13 @@ PGDATABASE_RENDER=gis
 
 OSMOSIS_BIN=/home/chentoz/Downloads/local-osmosis/bin/osmosis
 OSM2PGSQL_BIN=/usr/local/bin/osm2pgsql
-OSM2PGSQL_OPTIONS="-v -d $PGDATABASE_RENDER -G --hstore -r xml --tag-transform-script /home/chentoz/Downloads/openstreetmap-carto/openstreetmap-carto.lua -C 9999 --number-processes 5 -S /home/chentoz/Downloads/openstreetmap-carto/openstreetmap-carto.style"
+OSM2PGSQL_OPTIONS="-v -d $PGDATABASE_RENDER -G --hstore-all -r xml --tag-transform-script /home/chentoz/Downloads/openstreetmap-carto/openstreetmap-carto.lua -C 9999 --number-processes 5 -S /home/chentoz/Downloads/openstreetmap-carto/openstreetmap-carto.style"
 
 RENDER_BIN=/usr/local/bin/render_expired
-RENDER_OPTIONS="-m $MAPSTYLE -z $EXPIRY_MINZOOM -Z $EXPIRY_MAXZOOM -n 5"
-# RENDER_OPTIONS="-m $MAPSTYLE -z $EXPIRY_MINZOOM -Z $EXPIRY_MAXZOOM -d $EXPIRY_MINZOOM -n 5"
+RENDER_OPTIONS="-m $MAPSTYLE -z $EXPIRY_MINZOOM -Z $EXPIRY_MAXZOOM -n 10"
+
+EXPIRE_BIN="ruby /home/chentoz/Downloads/tile_expiry/update.rb"
+EXPIRE_OPTIONS=
 
 BASE_DIR=/tmp
 LOG_DIR=/tmp
@@ -44,11 +44,8 @@ PGSQLLOG=$LOG_DIR/osm2pgsql.log
 EXPIRYLOG=$LOG_DIR/expiry.log
 RUNLOG=$LOG_DIR/run.log
 
-EXPIRY_MINZOOM=14
-EXPIRY_MAXZOOM=20
-
-#*************************************************************************
-#*************************************************************************
+EXPIRY_MINZOOM=16
+EXPIRY_MAXZOOM=19
 
 m_info()
 {
@@ -57,8 +54,9 @@ m_info()
 
 m_debug()
 {
-        # return 0
-        echo "[`date +"%Y-%m-%d %H:%M:%S"`] $$ $1" >> "$RUNLOG"
+        if [ "$DEBUG" = "on" ] ; then
+            echo "[`date +"%Y-%m-%d %H:%M:%S"`] $$ $1" >> "$RUNLOG"
+        fi
 }
 
 m_error()
@@ -114,54 +112,48 @@ if [ $# -eq 1 ] ; then
         exit 2
     fi
 
-    m_info "============================= Updating : $(date -u +"%Y-%m-%d_%H:%M:%S") ============================="
-    m_ok "diff openstreetmap database changes between $MIN_AGO and $NOW dates."
+    m_info "============================= Updating : $(date +"%Y-%m-%d_%H:%M:%S") ============================="
+    m_ok "gis edit changes between $MIN_AGO and $NOW"
     m_debug "$OSMOSIS_BIN --read-apidb-change host=$PGHOST database=$PGDATABASE_EDIT  user=$PGUSER password=$PGPASS \
 	    intervalBegin=$MIN_AGO intervalEnd=$NOW validateSchemaVersion=no --write-xml-change file=$CHANGE_FILE"
     if ! $OSMOSIS_BIN --read-apidb-change host="$PGHOST" database="$PGDATABASE_EDIT"  user="$PGUSER" password="$PGPASS" \
 	    intervalBegin=$MIN_AGO intervalEnd=$NOW validateSchemaVersion="no" --write-xml-change file="$CHANGE_FILE" 1>&2 2> "$OSMOSISLOG" ; then
         m_error "Osmosis error"
     fi
-
     m_info "Change file $CHANGE_FILE, lines : $(wc -l $CHANGE_FILE | cut -f1 -d' ')"
 
-    if [ $(grep judyzhu $CHANGE_FILE 2>/dev/null | wc -l) -le 0 ] ; then
-
-        m_ok "importing diff"
-        EXPIRY_METAZOOM=`expr $EXPIRY_MAXZOOM - 0`
-        m_debug "$OSM2PGSQL_BIN -a --slim -e$EXPIRY_MINZOOM-$EXPIRY_MAXZOOM $OSM2PGSQL_OPTIONS -o "$EXPIRY_FILE" $CHANGE_FILE"
-        if ! $OSM2PGSQL_BIN -a --slim -e$EXPIRY_MINZOOM-$EXPIRY_MAXZOOM $OSM2PGSQL_OPTIONS -o "$EXPIRY_FILE" $CHANGE_FILE 1>&2 2> "$PGSQLLOG" ; then
-            m_error "osm2pgsql error"
-        fi
-
-        cat $EXPIRY_FILE | sort | uniq | tee -a > $EXPIRY_FILE
-
-        m_ok "generating dirty tiles url"
-        rm /tmp/render.list
-        touch /tmp/render.list
-        while IFS= read -r line; do
-            m_info "dirty tiles : $line"
-            # echo "http://localhost/$line.png" >> $(($EXPIRY_FILE)_url)
-            var1=$(echo $line | cut -f1 -d/)
-            var2=$(echo $line | cut -f2 -d/)
-            var3=$(echo $line | cut -f3 -d/)
-            echo "$var2 $var3 $var1" >> /tmp/render.list
-        done < $EXPIRY_FILE
-
-        m_ok "expiring tiles"
-        if ! $RENDER_BIN $RENDER_OPTIONS < $EXPIRY_FILE 2>&1 | tail -8 >> "$EXPIRYLOG"; then
-            m_info "Expiry failed"
-        fi
-
-        m_ok "Dirty tile : $EXPIRY_FILE, lines : $(wc -l $EXPIRY_FILE | cut -f1 -d' ')"
-    else
-
-        m_info "importing *delete* diff / expiry manually"
+    m_ok "updating render database : ${PGDATABASE_RENDER}"
+    m_debug "$OSM2PGSQL_BIN -a --slim $OSM2PGSQL_OPTIONS -e$EXPIRY_MINZOOM-$EXPIRY_MAXZOOM -o "$EXPIRY_FILE" $CHANGE_FILE"
+    if ! $OSM2PGSQL_BIN -a --slim $OSM2PGSQL_OPTIONS -e$EXPIRY_MINZOOM-$EXPIRY_MAXZOOM -o "$EXPIRY_FILE" $CHANGE_FILE; then
+        m_error "osm2pgsql error"
     fi
 
-    m_info "============================= Updating Done : $(date -u +"%Y-%m-%d_%H:%M:%S") ============================="
-    freelock "$LOCK_FILE"
+    cat $EXPIRY_FILE | sort | uniq | tee -a > $EXPIRY_FILE
+    m_ok "Dirty tiles (Add/Modity) : $EXPIRY_FILE, lines : $(wc -l $EXPIRY_FILE | cut -f1 -d' ')"
 
-    set > /tmp/set-output2
+    # m_ok "generating dirty tiles url"
+    # rm /tmp/render.list
+    # touch /tmp/render.list
+    # while IFS= read -r line; do
+        #m_info "dirty tiles : $line"
+        # echo "http://localhost/$line.png" >> $(($EXPIRY_FILE)_url)
+        #var1=$(echo $line | cut -f1 -d/)
+        #var2=$(echo $line | cut -f2 -d/)
+        #var3=$(echo $line | cut -f3 -d/)
+        #echo "$var2 $var3 $var1" >> /tmp/render.list
+    #done < $EXPIRY_FILE
+
+    m_ok "expiring tiles : using expired list"
+    if ! $RENDER_BIN $RENDER_OPTIONS<$EXPIRY_FILE ; then
+        m_info "render_expired expiry failed"
+    fi
+
+    m_ok "expiring tiles : using with larger zoom"
+    if ! $EXPIRE_BIN $EXPIRE_OPTIONS $CHANGE_FILE ; then
+        m_info "expire.rb expiry failed"
+    fi
+
+    m_info "============================= Updating Done : $(date +"%Y-%m-%d_%H:%M:%S") ============================="
+    freelock "$LOCK_FILE"
 
 fi
